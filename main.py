ed8113d0f738c1eb0148defde6628f213f5bec0e
rimprog <riminprog@gmail.com>
2020-04-17 17:06:55 +0300

    Create django filters

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ from datacenter.models import Passcard, Visit
 
 
 if __name__ == "__main__":
-    # Программируем здесь
+    passcards = Passcard.objects.all()
+    first_passcard = passcards[2]
+    print(first_passcard.owner_name)
+    print(first_passcard.passcode)
+    print(first_passcard.created_at)
+    print(first_passcard.is_active)
+
     print('Количество пропусков:', Passcard.objects.count())
+
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print('Активных пропусков:', len(active_passcards))
